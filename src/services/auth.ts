949a3ba7e8afbb43a84699f64316933b6b07786e
André Araujo <andre.arajo07@gmail.com>
2025-06-22 13:17:59 -0300

    feat: Variaveis no frontend

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { config } from '../config/env';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1';
+const API_URL = config.API_URL;
 
 // Interface para o resultado do login
 export interface LoginResponse {
